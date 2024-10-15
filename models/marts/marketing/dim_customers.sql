17923668a5b8d8bced5d1c9e4eecfa843627f0eb
Brian Teodoro <brian.teodoro@dbtlabs.com>
2024-10-15 15:02:22 +0000

    added source yml for jaffle_shop customers and orders.

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -14,7 +14,6 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
@@ -34,8 +33,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce (customer_orders.number_of_orders, 0) 
-        as number_of_orders,
+        coalesce (customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
 
     from customers

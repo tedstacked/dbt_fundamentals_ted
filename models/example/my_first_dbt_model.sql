cbf2f09016f88c89b8032e68f694fa50e0686636
Brian Teodoro <brian.teodoro@dbtlabs.com>
2025-07-21 11:12:25 +0000

    testing ci

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,12 +11,17 @@
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select 7 as id
-    union all
-    select 8 as id
-
+select 1 as id, 'BR' as region, 10 as weight, '1970-01-01'::date as updated_at
+union
+select 2 as id, 'WS' as region, 2 as weight, '1970-01-01'::date as updated_at
+union
+select 3 as id, 'WS' as region, 1 as weight, '1970-01-01'::date as updated_at
+union
+select 5 as id, 'WS' as region, 5 as weight, '1970-01-01'::date as updated_at
+union
+select 6 as id, 'WS' as region, 6 as weight, '1970-01-01'::date as updated_at
+union
+select 7 as id, 'WS' as region, 123 as weight, '1970-01-01'::date as updated_at
 )
 
 select *

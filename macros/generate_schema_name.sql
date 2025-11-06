9600c9174bb4dab6008ebe9c4140fe8be488cb62
Brian Teodoro <brian.teodoro@dbtlabs.com>
2025-11-06 15:52:02 +0000

    hello

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,18 +1,18 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
+-- {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
+--     {%- set default_schema = target.schema -%}
+--     {%- if custom_schema_name is none -%}
 
-        {{ default_schema }}
+--         {{ default_schema }}
         
-    {%- elif target.name == 'ci' -%}
+--     {%- elif target.name == 'ci' -%}
 
-        {{ custom_schema_name }}_{{ default_schema | trim }}
+--         {{ custom_schema_name }}_{{ default_schema | trim }}
 
-    {%- else -%}
+--     {%- else -%}
 
-        {{ default_schema }}_{{ custom_schema_name | trim }}
+--         {{ default_schema }}_{{ custom_schema_name | trim }}
 
-    {%- endif -%}
+--     {%- endif -%}
 
-{%- endmacro %}
+-- {%- endmacro %}
--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,9 @@
 
 -- Use the `ref` function to select from other models
 
-select *
+{# select *
 from {{ ref('my_first_dbt_model') }}
-where id = 1
+where id = 1 #}
+
+
+This is a comment

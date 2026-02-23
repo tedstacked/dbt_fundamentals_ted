f799002173367b944390345d530e0ae8448c435d
Ted <61456560+tedstacked@users.noreply.github.com>
2026-02-24 04:13:19 +0800

    Revert "test for macro changes if it will build models that depend on it"

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,15 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
-{{ config(materialized='table') }}
-
--- This model depends on my_first_dbt_model via ref()
--- AND uses the same macro
-
-select 
-    id,
-    name,
-    prefixed_name,
-    {{ add_prefix('name') }} as prefixed_name_again
+select *
 from {{ ref('my_first_dbt_model') }}
 where id = 1
